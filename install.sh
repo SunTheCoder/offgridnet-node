031c5f402fff15d078196ac7db823e5b979fd1ed
SunTheCoder <sunenglishjr@gmail.com>
2025-04-15 13:16:49 -0400

    kiwix setup

--- install.sh
+++ install.sh
@@ -40,11 +40,11 @@ chown sunny:sunny /home/sunny/kiwix/data/library.xml
 
 # Copy systemd service files
 echo "Copying systemd service files..."
-cp systemd/hostapd.service /etc/systemd/system/
-cp systemd/dnsmasq.service /etc/systemd/system/
-cp systemd/kiwix.service /etc/systemd/system/
-cp systemd/set-wlan-ip.service /etc/systemd/system/
-cp systemd/offgridnet.service /etc/systemd/system/
+cp /home/sunny/offgridnet-node/systemd/hostapd.service /etc/systemd/system/
+cp /home/sunny/offgridnet-node/systemd/dnsmasq.service /etc/systemd/system/
+cp /home/sunny/offgridnet-node/systemd/kiwix.service /etc/systemd/system/
+cp /home/sunny/offgridnet-node/systemd/set-wlan-ip.service /etc/systemd/system/
+cp /home/sunny/offgridnet-node/systemd/offgridnet.service /etc/systemd/system/
 
 # Enable and start services in correct order
 echo "Enabling and starting services..."
